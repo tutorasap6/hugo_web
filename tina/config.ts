ba8b019f3e8e072ee1973bbde1a687edcb5e19ec
sharmiansi <sharmiansi@gmail.com>
2024-02-03 19:58:07 +0530

    Tina cms config - about page

--- tina/config.ts
+++ tina/config.ts
@@ -201,6 +201,263 @@ export default defineConfig({
         ],
       },
 
+      {
+        name: "about",
+        label: "About Us",
+        path: "content/english",
+        ui: {
+          allowedActions: {
+            create: false,
+            delete: false,
+          },
+        },
+        match: {
+          include: "about*",
+        },
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            description: "Enter the description",
+          },
+          {
+            type: "string",
+            name: "layout",
+            label: "layout name",
+            description: "It is default value - DO NOT CHANGE",
+          
+          },
+          
+          // sub section
+          {
+            type: "object",
+            name: "about_us",
+            label: "About Us",
+            fields: [
+              {
+                type: "boolean",
+                name: "enable",
+                label: "Enable Section",
+              },
+              {
+                type: "image",
+                name: "image",
+                label: "Upload Image",
+              },
+              {
+                type: "string",
+                name: "title",
+                label: "Title",
+              },
+              {
+                type: "string",
+                name: "subtitle",
+                label: "Subtitle",
+              },
+              {
+                type: "string",
+                name: "content",
+                label: "Content",
+              },
+              
+            ],
+          },
+          // sub section - facts
+          {
+            type: "object",
+            name: "facts",
+            label: "Facts Section",
+            fields: [
+                  {
+                    type: "boolean",
+                    name: "enable",
+                    label: "Enable or Disable",
+                  },
+                  {
+                    type: "object",
+                    name: "fact_item",
+                    label: "Fact Items",
+                    list:true,
+                    fields: [
+                      {
+                        type: "string",
+                        name: "title",
+                        label: "Title",
+                      },
+                      {
+                        type: "string",
+                        name: "counter",
+                        label: "Counter",
+                      },
+                      {
+                        type: "string",
+                        name: "counter_append",
+                        label: "Counter Append",
+                      },
+                      {
+                        type: "string",
+                        name: "counter_prepend",
+                        label: "Counter Prepend",
+                      },
+                      {
+                        type: "string",
+                        name: "description",
+                        label: "Description",
+                      },
+                      
+                    ],
+                    itemProps: (item) => ({
+                      label: item.title, // Use the question as the display label
+                    }),
+                  },
+                ],
+             },
+        // sub section for our story
+        {
+          type: "object",
+          name: "our_story",
+          label: "Our story Section",
+          fields: [
+                {
+                  type: "boolean",
+                  name: "enable",
+                  label: "Enable or Disable",
+                },
+                {
+                  type: "string",
+                  name: "title",
+                  label: "Title",
+                },
+                {
+                  type: "string",
+                  name: "subtitle",
+                  label: "Subtitle",
+                },
+                {
+                  type: "string",
+                  name: "content",
+                  label: "Content",
+                },
+                {
+                  type: "object",
+                  name: "image",
+                  label: "Image",
+                  list:true,
+                  fields: [
+                    {
+                      type: "image",
+                      name: "item",
+                      label: "Images",
+                      description: "Upload multiple images",
+                      
+                    },
+                  ],
+                  
+                },
+                
+                
+                
+            ],
+            itemProps: (item) => ({
+              label: item.title, // Use the question as the display label
+            }),
+                  
+           },
+
+        // sub section teams 
+
+        {
+          type: "object",
+          name: "team",
+          label: "Team Section",
+          fields: [
+            {
+              type: "boolean",
+              name: "enable",
+              label: "Enable or Disable",
+            },
+            {
+              type: "string",
+              name: "title",
+              label: "Title",
+            },
+            {
+              type: "string",
+              name: "subtitle",
+              label: "Subtitle",
+            },
+            {
+              type: "string",
+              name: "content",
+              label: "Content",
+            },
+            {
+              type: "object",
+              name: "team_item",
+              label: "Team Members",
+              list:true,
+              fields: [
+                {
+                  type: "string",
+                  name: "name",
+                  label: "Name",
+                },
+                {
+                  type: "string",
+                  name: "designation",
+                  label: "Designation",
+                },
+                {
+                  type: "image",
+                  name: "image",
+                  label: "Image",
+                },
+                {
+                  type: "string",
+                  name: "description",
+                  label: "Description",
+                },
+                
+                {
+                  type: "object",
+                  name: "social_links",
+                  label: "Social Links",
+                  list:true,
+                  fields: [
+                    {
+                      type: "string",
+                      name: "icon",
+                      label: "Name",
+                    },
+                    {
+                      type: "string",
+                      name: "link",
+                      label: "link",
+                    },
+                    
+                    
+                  ],
+                },
+              ],
+              itemProps: (item) => ({
+                label: item.name, // Use the name
+              }),
+            },
+          ],
+         
+        },
+    
+        ],
+      },
+
     ],
   },
 });
